2c7994d16007da164ee621a8e656f64505707ed9
diogomatos97 <diogopsnskullcrap@gmail.com>
2020-08-26 09:50:25 +0100

    Radio Project

---
+++ RadioAppStarterCode/RadioApp/RadioApp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace RadioApp
+{
+    public class Radio
+    {
+        private int _channel = 1;
+        private bool _on = false;
+
+        public int Channel
+        {
+            get { return _channel; }
+            set { if (_on == true & value > 0 & value <=4) _channel = value; }
+        }
+
+        public string Play() {
+            if (_on == false)
+            {
+                return "Radio is off";
+            }
+            else
+            {
+                
+                  return $"Playing channel {Channel}";
+                
+            }
+
+        }
+        public void TurnOff()
+        {
+            if (_on == true)
+            {
+                _on = false;
+              
+            }
+
+        }
+        public void TurnOn() {
+            if (_on == false)
+            {
+                _on = true;
+            }
+        
+        }
+
+        public string ChannelOn(int num)
+        {
+            switch (num)
+            {
+                case 2:
+                    _channel = num;
+                    return "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio2_mf_p";
+
+                case 4:
+                    _channel = num;
+                    return "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1xtra_mf_p";
+                case 1:
+                    _channel = num;
+                    return "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p";
+                case 3:
+                    _channel = num;
+                    return "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio3_mf_p";
+            }
+            return "";
+        }
+        public string Name(int num)
+        {
+            switch (num)
+            {
+                case 1:
+                    return "BBC Radio 1";
+                case 2:
+                    return "BBC Radio 2";
+                case 3:
+                    return "BBC Radio 3";
+                case 4:
+                    return "BBC 1Xtra";
+            }
+            return "";
+        }
+
+    }
+    // implement a class Radio that corresponds to the Class diagram 
+    //   and specification in the Radio_Mini_Project document
+}
---
+++ RadioAppStarterCode/RadioGui/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using RadioApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppTest
+{
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Radio r = new Radio();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+          
+            Player.Play();
+        }
+
+        private void Off1_Click(object sender, RoutedEventArgs e)
+        {
+            Player.Stop();
+        }
+
+        private void volumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (volumeSlider != null)
+            {
+
+
+                Player.Volume = volumeSlider.Value / 100;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (sender.Equals(Radio1))
+            {
+                RName.Text = r.Name(1);
+                Player.Source = new Uri(r.ChannelOn(1), UriKind.RelativeOrAbsolute);
+                Player.Play();
+            }
+            else if (sender.Equals(Radio2))
+            {
+                RName.Text = r.Name(2);
+                Player.Source = new Uri(r.ChannelOn(2), UriKind.RelativeOrAbsolute);
+                Player.Play();
+
+            }
+            else if (sender.Equals(Radio3)) {
+                RName.Text = r.Name(3);
+                Player.Source = new Uri(r.ChannelOn(3), UriKind.RelativeOrAbsolute);
+                Player.Play();
+            }
+            else {
+                RName.Text = r.Name(4);
+                Player.Source = new Uri(r.ChannelOn(4), UriKind.RelativeOrAbsolute);
+                Player.Play();
+            }
+          
+            
+        }
+
+        
+    }
+}
